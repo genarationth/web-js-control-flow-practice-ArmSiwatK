bf5e6cdecdc77682e7b385162d368a62dd3290b3
Siwat Kanjanapanitkun <125792234+ArmSiwatK@users.noreply.github.com>
2023-03-13 13:41:59 +0700

    Assignment Turn In

---
+++ webJSControlFlowPracticeArmSiwatK.js
@@ -0,0 +1,30 @@
+const dayNumber = Math.floor(Math.random() * 10);
+let day = '';
+
+switch (dayNumber) {
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+        break;
+    default:
+        day = "Invalid day number";
+}
+
+console.log(`The day is ${day}.`);
